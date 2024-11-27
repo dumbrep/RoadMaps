dc4114fdf4df3e7af891113f3a2166016cef9c1f
dumbrep <115614546+dumbrep@users.noreply.github.com>
2024-11-27 19:05:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ open_token = st.secrets["OPENAI_KEY"]
 os.environ["OPENAI_API_KEY"] = open_token
 
 # Initialize model 
-model = ChatOpenAI(model="gpt-4 mini")
+model = ChatOpenAI(model="ada")
 
 #Tools declarations
 @tool
